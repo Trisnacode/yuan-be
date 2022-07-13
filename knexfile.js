ec41d0e2068a9a28420232a6f008beea0c6c08c1
mcimam <imam123.ci@gmail.com>
2022-07-13 11:27:14 +0700

    change orm

---
+++ knexfile.js
@@ -0,0 +1,50 @@
+// Update with your config settings.
+require('dotenv').config();
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'mysql2',
+    connection: {
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+    },
+    ssl: {
+      rejectUnauthorized: false,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: 'db/migrations',
+    },
+    seeds: {
+      directory: 'db/seeds',
+    },
+  },
+  production: {
+    client: 'mysql2',
+    connection: {
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: 'db/migrations',
+    },
+  },
+
+};
